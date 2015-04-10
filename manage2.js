0935139e296a96318cf15d738a5d4a9e64a77840
stefanocudini <stefano.cudini@gmail.com>
2015-04-10 17:27:46 +0200

    adde templates pills

--- manage2.js
+++ manage2.js
@@ -11,25 +11,22 @@ require([
     Compiler.resolve([menuConfig], {
             placeholders: {
             	FENIX_CDN: "//fenixapps.fao.org/repository",
-            	FENIX_CONF_NLS: "../../../nls"
+            	FENIX_I18N: "../../../i18n"
             },
             config: {
                 locale: 'en',
                 paths: {
+					text: "{FENIX_CDN}/js/requirejs/plugins/text/2.0.12/text",
+					i18n: "{FENIX_CDN}/js/requirejs/plugins/i18n/2.0.4/i18n",
+					domready: "{FENIX_CDN}/js/requirejs/plugins/domready/2.0.1/domReady",
+
                     underscore: "{FENIX_CDN}/js/underscore/1.7.0/underscore.min",
-                    backbone:   "{FENIX_CDN}/js/backbone/1.1.2/backbone.min",
                     handlebars: "{FENIX_CDN}/js/handlebars/2.0.0/handlebars",
-                    amplify:    "{FENIX_CDN}/js/amplify/1.1.2/amplify.min",
-                    config: 'config',
-                    html: 'html'
+                    amplify:    "{FENIX_CDN}/js/amplify/1.1.2/amplify.min"
                 },
                 shim: {
                     underscore: {
                         exports: '_'
-                    },
-                    backbone: {
-                        deps: ['underscore', 'jquery'],
-                        exports: 'Backbone'
                     },
                     handlebars: {
                         exports: 'Handlebars'
@@ -45,11 +42,21 @@ require([
     // Bootstrap the application
     require([
         'fx-menu/start',
-        'src/components/AuthenticationManager',
-        'fx-editor/start',
+        'submodules/fenix-ui-common/js/AuthManager',
+        'handlebars',
         'amplify',
-        'domReady!'
-    ], function (TopMenu, AuthenticationManager, Editor) {
+
+        'text!config/services.json',
+
+		'text!html/pills.html',
+
+        'domready!'
+    ], function (TopMenu, AuthManager, Handlebars, Amplify,
+
+    	Config,
+    	tmplPills) {
+
+    	pillsTmpl = Handlebars.compile(tmplPills);
 
         var authUser = amplify.store.sessionStorage('afo.security.user'),
             menuUrl,
@@ -70,7 +77,7 @@ require([
         });
         
         /*Login*/
-        new AuthenticationManager();
+        new AuthManager();
 
         amplify.subscribe('login', function (user) {
             refreshMenu(authMenuConfig);
@@ -92,26 +99,21 @@ require([
             })
         }
 
+		$('#pills-quest').html(pillsTmpl({
+			items: [
+				{id:1, title: "ciao1", active: true, html:'<b>HTML1</b>'},
+				{id:2, title: "ciao2", active: false, html:'<b>HTML2</b>'},
+				{id:3, title: "ciao3", active: false, html:'<b>HTML3</b>'}
+			]
+		}) );
 
-        //Editor Metadata
-        var userConfig = {
-            container: "div#metadataEditorContainer",
-            source: null,
-            resourceType: 'dataset', //dataset, geographic, codelist
-            readOnly: false,
-            widget: {
-                lang: 'EN'
-            },
-            config: {
-                gui: 			"config/fenix-ui-metadata-editor/gui-config.json",
-                validation: 	"submodules/fenix-ui-metadata-editor/conf/json/fx-editor-validation-config.json",
-                jsonMapping: 	"submodules/fenix-ui-metadata-editor/conf/json/fx-editor-mapping-config.json",
-                ajaxEventCalls: "submodules/fenix-ui-metadata-editor/conf/json/fx-editor-ajax-config.json",
-                dates: 			"submodules/fenix-ui-metadata-editor/conf/json/fx-editor-dates-config.json"
-            }
-        };
 
-        this.editor = new Editor().init(userConfig);
+        $('#form-contact').on('submit', function(e) {
+			e.preventDefault();
+			var form$ = $(this);
+			form$.parents('.container').slideUp();
+			console.log( form$.serialize() );
+        });
 
     });
 });
