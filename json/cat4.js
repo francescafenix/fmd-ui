9136dff07e29994a7e14ff25f3a833a4fa6ba6f0
stefanocudini <stefano.cudini@gmail.com>
2015-04-30 15:49:13 +0200

    ask4

--- json/cat4.js
+++ json/cat4.js
@@ -4,60 +4,55 @@ define(['i18n!nls/questions'], function(Quests) {
 		"title": Quests.cat4,
 		"properties": {
 			"ask4": {
-				"title": Quests['ask4'],
-				"type": "string",
-				"uniqueItems": true,
-				"format": "select",
-				"enum": [
-					"Yes",
-					"No",
-					"Partial"
-				]
-			},
-			"ask4.1": {
-				"title": Quests['ask4.1'],
-				"type": "string",
-				"uniqueItems": true,
-				"format": "select",
-				"enum": [
-					"Yes",
-					"No",
-					"Partial"
-				]
-			},
-			"ask4.1.1": {
-				"title": Quests['ask4.1.1'],
-				"type": "string"
-			},
-			"ask4.2": {
-				"title": Quests['ask4.2'],
-				"type": "string",
-				"uniqueItems": true,
-				"format": "select",
-				"enum": [
-					"Yes",
-					"No",
-					"Partial"
-				]
-			},
-			"ask4.2.1": {
-				"title": Quests['ask4.2.1'],
-				"type": "string"
-			},
-			"ask4.3": {
-				"title": Quests['ask4.3'],
-				"type": "string",
-				"uniqueItems": true,
-				"format": "select",
-				"enum": [
-					"Yes",
-					"No",
-					"Partial"
-				]
-			},
-			"ask4.3.1": {
-				"title": Quests['ask4.3.1'],
-				"type": "string"
+				"type":"object",
+				"title": Quests['ask4'],				
+				"properties": {
+					"ask4.1": {
+						"title": Quests['ask4.1'],
+						"type": "string",
+						"uniqueItems": true,
+						"format": "select",
+						"enum": [
+							"Yes",
+							"No",
+							"Partial"
+						]
+					},
+					"ask4.1.1": {
+						"title": Quests['ask4.1.1'],
+						"type": "string"
+					},
+					"ask4.2": {
+						"title": Quests['ask4.2'],
+						"type": "string",
+						"uniqueItems": true,
+						"format": "select",
+						"enum": [
+							"Yes",
+							"No",
+							"Partial"
+						]
+					},
+					"ask4.2.1": {
+						"title": Quests['ask4.2.1'],
+						"type": "string"
+					},
+					"ask4.3": {
+						"title": Quests['ask4.3'],
+						"type": "string",
+						"uniqueItems": true,
+						"format": "select",
+						"enum": [
+							"Yes",
+							"No",
+							"Partial"
+						]
+					},
+					"ask4.3.1": {
+						"title": Quests['ask4.3.1'],
+						"type": "string"
+					}
+				}
 			}
 		}
 	};
